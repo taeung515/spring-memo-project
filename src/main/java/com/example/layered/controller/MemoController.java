09a2536a4ccdea43a2be4dadc25fdd05330b8623
taeung515 <xodndd515@gmail.com>
2025-05-11 15:47:11 +0900

    feat: 메모 생성 api, 3 Layered Architecture

---
+++ src/main/java/com/example/layered/controller/MemoController.java
@@ -0,0 +1,35 @@
+package com.example.layered.controller;
+
+import com.example.layered.dto.MemoRequestDto;
+import com.example.layered.dto.MemoResponseDto;
+import com.example.layered.service.MemoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/memos") // Prefix
+public class MemoController {
+
+    // 주입된 의존성을 변경할 수 없어 객체의 상태를 안전하게 유지할 수 있다.
+    private final MemoService memoService;
+
+    /**
+     * 생성자 주입
+     * 클래스가 필요로 하는 의존성을 생성자를 통해 전달하는 방식
+     * @param memoService @Service로 등록된 MemoService 구현체인 Impl
+     */
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto) {
+
+        // Service Layer 호출, 응답
+        return new ResponseEntity<>(memoService.saveMemo(dto), HttpStatus.CREATED);
+    }
+}
